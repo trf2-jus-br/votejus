ca6037adaa1b64a73d1d36634ba500c64eea0b42
Renato Crivano <renato.crivano@gmail.com>
2023-01-25 14:25:15 -0300

    Parando de logar envio de emails com sucesso

--- utils/mailer.jsx
+++ utils/mailer.jsx
@@ -23,7 +23,7 @@ export default {
             if (err) {
                 console.log(err)
             } else {
-                console.log(info);
+                // console.log(info);
             }
         })
     },
